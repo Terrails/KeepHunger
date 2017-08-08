db95db1bdf564441b4a6f8b274557e7c0f4c8938
Terrails <terrails.business@outlook.com>
2017-08-08 15:26:23 +0200

    1.12 Update and Some Improvements

---
+++ 1.11.2/src/main/java/terrails/statskeeper/config/ConfigFactoryGUI.java
@@ -0,0 +1,40 @@
+package terrails.statskeeper.config;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraftforge.fml.client.IModGuiFactory;
+
+import java.util.Set;
+
+public class ConfigFactoryGUI implements IModGuiFactory {
+
+    public boolean hasConfigGui() {
+        return true;
+    }
+
+    public GuiScreen createConfigGui(GuiScreen parentScreen) {
+        return new GUIConfig(parentScreen);
+    }
+
+    @Override
+    public void initialize(Minecraft minecraftInstance) {
+    }
+
+    @SuppressWarnings( "deprecation" )
+    @Override
+    public Class<? extends GuiScreen> mainConfigGuiClass() {
+        return GUIConfig.class;
+    }
+
+    @SuppressWarnings( "deprecation" )
+    @Override
+    public Set<RuntimeOptionCategoryElement> runtimeGuiCategories() {
+        return null;
+    }
+
+    @SuppressWarnings( "deprecation" )
+    @Override
+    public RuntimeOptionGuiHandler getHandlerFor(RuntimeOptionCategoryElement element) {
+        return null;
+    }
+}
